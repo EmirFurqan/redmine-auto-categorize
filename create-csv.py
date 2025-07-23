b379c8e7f3c62b7dd429f77ecf86702a3cb94ad8
EmirFurqan <emirfurqanyt@hotmail.com>
2025-07-23 14:22:43 +0300

    added

---
+++ create-csv.py
@@ -0,0 +1,43 @@
+import requests
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+# Ortam değişkenlerini yükle
+load_dotenv()
+
+BASE_URL = os.getenv("REDMINE_BASE_URL")
+HEADERS = {
+    "Content-Type": "application/json",
+    "X-Redmine-API-Key": os.getenv("REDMINE_API_KEY"),
+}
+
+def get_issues():
+    response = requests.get(f"{BASE_URL}/issues.json?limit=100", headers=HEADERS)
+    if response.status_code != 200:
+        print(f"Issue verileri alınamadı. Hata kodu: {response.status_code}")
+        return []
+    return response.json().get("issues", [])
+
+def export_to_csv(issues, filename="veri.csv"):
+    rows = []
+    for issue in issues:
+        title = issue.get("subject", "")
+        description = issue.get("description", "")
+        title_description = f"{title}\n\n{description}"
+
+        project = issue.get("project", {}).get("name", "")
+
+        rows.append([title_description, project])
+
+    df = pd.DataFrame(rows, columns=["text", "label"])
+    df.to_csv(filename, index=False, encoding="utf-8-sig")
+    print(f"CSV dosyası oluşturuldu: {filename}")
+
+def main():
+    issues = get_issues()
+    export_to_csv(issues)
+
+
+if __name__ == "__main__":
+    main()
